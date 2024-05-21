14383a70cd67d31f8b64156ece7b1593b10ec864
djedme22 <edisson.medinab@uqvirtual.edu.co>
2024-05-21 18:00:13 -0500

    Mejora del Menu, y vinculacion con los componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,24 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './componentes/header/header.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
+import { CategoriasComponent } from './componentes/categorias/categorias.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { NegociosPendientesComponent } from './componentes/negocios-pendientes/negocios-pendientes.component';
 import { RegistroClienteComponent } from './componentes/registro-cliente/registro-cliente.component';
 import { GestionNegociosComponent } from './componentes/gestion-negocios/gestion-negocios.component';
+import { FavoritosComponent } from './componentes/favoritos/favoritos.component';
 import { CrearNegocioComponent } from './componentes/crear-negocio/crear-negocio.component';
 import { DetalleNegocioComponent } from './componentes/detalle-negocio/detalle-negocio.component';
 
 export const routes: Routes = [
 { path: 'header', component: HeaderComponent},
 { path: 'inicio', component: InicioComponent },
+{ path: 'categorias', component: CategoriasComponent },
 { path: 'login', component: LoginComponent },
 { path: 'registro-cliente', component: RegistroClienteComponent },
+{ path: 'negocios-pendientes', component: NegociosPendientesComponent },
 { path: 'gestion-negocios', component: GestionNegociosComponent},
+{ path: 'favoritos', component: FavoritosComponent},
 { path: 'crear-negocio', component: CrearNegocioComponent},
 { path: 'detalle-negocio/:codigo', component: DetalleNegocioComponent},
 { path: "**", pathMatch: "full", redirectTo: "" }
---
+++ src/app/componentes/negocios-pendientes/negocios-pendientes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-negocios-pendientes',
+  standalone: true,
+  imports: [],
+  templateUrl: './negocios-pendientes.component.html',
+  styleUrl: './negocios-pendientes.component.css'
+})
+export class NegociosPendientesComponent {
+
+}
